82b426135f130de7cf2491d70defb0e87264609a
Octavian <60541339+Octavian-imp@users.noreply.github.com>
2025-04-19 07:49:56 +0300

    Update index.tsx

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -16,7 +16,7 @@ const ListPage = () => {
   }
 
   useEffect(() => {
-    dispatch(fetchArticles({ offset: String(currentPage === 1 ? 0 : currentPage * articles.length) }))
+    dispatch(fetchArticles({ offset: String(currentPage === 1 ? 0 : (currentPage -1)* articles.length) }))
   }, [currentPage])
 
   if (articlesError !== null) {
