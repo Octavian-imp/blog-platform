c94219b18d64beda07ca0a9299b408294e626b08
octavian <sirghi1979@mail.ru>
2025-04-24 22:38:53 +0300

    refactoring

--- src/main/article/detail/index.tsx
+++ src/main/article/detail/index.tsx
@@ -1,20 +1,10 @@
 import ArticleItem from "@/components/ui/ArticleItem"
-import ArticlesApi from "@/services/ArticlesApi"
-import { TypeArticleItem } from "@/store/slices/Article"
-import { Flex, Skeleton } from "antd"
-import React, { useLayoutEffect, useState } from "react"
+import { Flex } from "antd"
+import React from "react"
 import { useParams } from "react-router"
 
 const DetailArticlePage = () => {
   const params: Readonly<Partial<{ slug: string }>> = useParams()
-
-  // if (!article) {
-  //   return (
-  //     <Flex style={{ maxWidth: "938px", alignSelf: "center", width: "100%" }} align="center" vertical gap={20}>
-  //       <Skeleton style={{ backgroundColor: "white", padding: "14px 15px", borderRadius: "5px" }} />
-  //     </Flex>
-  //   )
-  // }
 
   return (
     <Flex
